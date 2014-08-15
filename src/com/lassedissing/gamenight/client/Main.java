9876ab5a72474fbed208c03c5623e1a49c660ad9
Lasse <volpes@gmx.com>
2014-08-15 17:08:24 +0200

    Moved Main into seperate package

--- src/com/lassedissing/gamenight/client/Main.java
+++ src/com/lassedissing/gamenight/client/Main.java
@@ -3,7 +3,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-package com.lassedissing.gamenight;
+package com.lassedissing.gamenight.client;
 
 import com.jme3.app.SimpleApplication;
 import com.jme3.material.Material;
