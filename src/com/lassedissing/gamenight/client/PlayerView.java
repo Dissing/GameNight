33418d6d8700ab638a06f2503a9eb0162d3e2101
Lasse <volpes@gmx.com>
2014-08-28 19:31:52 +0200

    Implemented simple player model.

--- src/com/lassedissing/gamenight/client/PlayerView.java
+++ src/com/lassedissing/gamenight/client/PlayerView.java
@@ -17,22 +17,63 @@ import com.jme3.scene.shape.Box;
 public class PlayerView {
     
     private int id;
-    private Geometry geo;
-    
+    private Node node = new Node();;
     
     public PlayerView(int id, Node parent, Main app) {
         this.id = id;
-        Box b = new Box(0.8f,1.8f,0.8f);
-        geo = new Geometry("Player: " + id, b);
-        Material mat = new Material(app.getAssetManager(), "Common/MatDefs/Misc/Unshaded.j3md");
-        mat.setColor("Color", ColorRGBA.Blue);
-        geo.setMaterial(mat);   
-        geo.setLocalTranslation(0.8f, 18f, 0.8f);
-        parent.attachChild(geo);
+
+        Box headBox = new Box(0.15f,0.15f,0.15f);
+        Box torsoBox = new Box(0.2f,0.39f,0.15f);
+        Box armBox = new Box(0.03f,0.3f,0.03f);
+        Box legBox = new Box(0.036f,0.36f,0.036f);
+
+        Geometry headGeo = new Geometry("Player" + id + "head", headBox);
+        Geometry torsoGeo = new Geometry("Player" + id + "torso", torsoBox);
+        Geometry arm1Geo = new Geometry("Player" + id + "arm1", armBox);
+        Geometry arm2Geo = new Geometry("Player" + id + "arm2", armBox);
+        Geometry leg1Geo = new Geometry("Player" + id + "leg1", legBox);
+        Geometry leg2Geo = new Geometry("Player" + id + "leg2", legBox);
+
+        Material headMat = new Material(app.getAssetManager(), "Common/MatDefs/Misc/Unshaded.j3md");
+        Material torsoMat = new Material(app.getAssetManager(), "Common/MatDefs/Misc/Unshaded.j3md");
+        Material armMat = new Material(app.getAssetManager(), "Common/MatDefs/Misc/Unshaded.j3md");
+        Material legMat = new Material(app.getAssetManager(), "Common/MatDefs/Misc/Unshaded.j3md");
+
+        headMat.setColor("Color", new ColorRGBA(0.827f, 0.525f, 0.278f, 1.0f));
+        torsoMat.setColor("Color", new ColorRGBA(0.169f, 0.494f, 0.494f, 1.0f));
+        armMat.setColor("Color", new ColorRGBA(0.827f, 0.278f, 0.278f, 1.0f));
+        legMat.setColor("Color", new ColorRGBA(0.224f, 0.663f, 0.224f, 1.0f));
+
+        node.attachChild(headGeo);
+        headGeo.setLocalTranslation(0, headBox.yExtent+(torsoBox.yExtent + legBox.yExtent)*2, 0);
+        headGeo.setMaterial(headMat);
+
+        node.attachChild(torsoGeo);
+        torsoGeo.setLocalTranslation(0, torsoBox.yExtent + legBox.yExtent*2, 0);
+        torsoGeo.setMaterial(torsoMat);
+
+        node.attachChild(arm1Geo);
+        arm1Geo.setLocalTranslation(-0.3f, armBox.yExtent + legBox.yExtent*2, 0);
+        arm1Geo.setMaterial(armMat);
+
+        node.attachChild(arm2Geo);
+        arm2Geo.setLocalTranslation(0.3f, armBox.yExtent + legBox.yExtent*2, 0);
+        arm2Geo.setMaterial(armMat);
+
+        node.attachChild(leg1Geo);
+        leg1Geo.setLocalTranslation(-0.2f, legBox.yExtent, 0);
+        leg1Geo.setMaterial(legMat);
+
+        node.attachChild(leg2Geo);
+        leg2Geo.setLocalTranslation(0.2f, legBox.yExtent, 0);
+        leg2Geo.setMaterial(legMat);
+
+
+        parent.attachChild(node);
     }
     
     public void setPosition(Vector3f pos) {
-        geo.setLocalTranslation(pos);
+        node.setLocalTranslation(pos.subtractLocal(0, 1.6f, 0));
     }
     
 }
