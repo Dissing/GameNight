7837617bac2fb40941a4ad8be965a64646a827a1
Lasse <volpes@gmx.com>
2014-08-29 23:07:00 +0200

    Implemented world file persistance

--- src/com/lassedissing/gamenight/world/Chunk.java
+++ src/com/lassedissing/gamenight/world/Chunk.java
@@ -12,23 +12,23 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 
 @Serializable
-public class Chunk {
-    
+public class Chunk implements java.io.Serializable {
+
     public final transient static float BLOCK_SIZE = 1f;
     public final transient static int CHUNK_SIZE = 16;
     public final transient static int CHUNK_AREA = CHUNK_SIZE * CHUNK_SIZE;
     public final transient static int CHUNK_VOLUME = CHUNK_SIZE * CHUNK_SIZE * CHUNK_SIZE;
-    
+
     protected int[] blocks = new int[CHUNK_VOLUME];
     protected Vector3f location = new Vector3f();
-    
+
     /**
      * Only for serialization
      */
     public Chunk() {
-        
+
     }
-    
+
     public Chunk(int x, int z)  {
         for (int i = 0; i < CHUNK_SIZE; i++) {
             for (int j = 0; j < CHUNK_SIZE; j++) {
@@ -47,52 +47,52 @@ public class Chunk {
         }
         location.set(x, 0, z);
     }
-    
+
     public Vector3f getLocation() {
         return location;
     }
-    
+
     public int getX() {
         return (int)location.getX();
     }
-    
+
     public int getZ() {
         return (int)location.getZ();
     }
-    
+
     public int getIdAt(int x, int y, int z) {
         return blocks[(x & 0xF)*Chunk.CHUNK_AREA+y*Chunk.CHUNK_SIZE+(z & 0xF)];
     }
-    
+
     public Block getBlockAt(int x, int y, int z) {
         return new Block(this,(getX() << 4) + (x & 0xF), y & 0xFF , (getZ() << 4) + (z & 0xF));
     }
-    
+
     public void setIdAt(int value, int x, int y, int z) {
         blocks[(x & 0xF)*Chunk.CHUNK_AREA+y*Chunk.CHUNK_SIZE+(z & 0xF)] = value;
     }
- 
+
     public boolean isPopulated(int x, int y, int z) {
         int pos = x*CHUNK_AREA+y*CHUNK_SIZE+z;
-        
+
         //Check if block is outside chunk
         if (x >= CHUNK_SIZE || y >= CHUNK_SIZE || z >= CHUNK_SIZE || x < 0 || y < 0 || z < 0)
             return false;
-        
+
         //Check if block is not empty
         boolean res = blocks[pos] != 0;
         return res;
     }
-    
+
     private void writeObject(ObjectOutputStream oos) throws IOException {
         oos.defaultWriteObject();
-        
+
         for (int block : blocks) {
             oos.writeInt(block);
         }
         oos.writeObject(location);
     }
-    
+
     private void readObject(ObjectInputStream ois) throws ClassNotFoundException, IOException {
         ois.defaultReadObject();
         for (int i = 0; i < CHUNK_VOLUME; i++) {
@@ -100,5 +100,5 @@ public class Chunk {
         }
         location = (Vector3f) ois.readObject();
     }
-    
+
 }
--- src/com/lassedissing/gamenight/world/World.java
+++ src/com/lassedissing/gamenight/world/World.java
@@ -6,16 +6,26 @@
 package com.lassedissing.gamenight.world;
 
 import com.jme3.math.Vector3f;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.io.Serializable;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
 
+public class World implements Serializable {
 
-public class World {
-    
     private String name;
-    private Map<Long,Chunk> chunks = new HashMap<>();
-    
+    private HashMap<Long,Chunk> chunks = new HashMap<>();
+
+    public World(String name) {
+        this.name = name;
+    }
+
     public void generate(int width, int length) {
         for (int i = 0; i < width; i++) {
             for (int j = 0; j < length; j++) {
@@ -25,36 +35,87 @@ public class World {
             }
         }
     }
-    
-    public void load(String filename) {
-        
+
+    public static World load(String filename) {
+        FileInputStream in = null;
+        ObjectInputStream stream = null;
+        try {
+            in = new FileInputStream("levels/"+filename+".map");
+             stream = new ObjectInputStream(in);
+            return (World) stream.readObject();
+        } catch(Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                if (stream != null) {
+                    stream.close();
+                }
+                if (in != null) {
+                    in.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+        return null;
     }
-    
+
     public Block getBlockAt(int x, int y, int z) {
         return getChunkAt(x >> 4, z >> 4).getBlockAt(x & 0xF, y, z & 0xF);
     }
-    
+
     public Block getBlockAt(Vector3f position) {
         return getBlockAt((int)position.x, (int)position.y, (int)position.z);
     }
-    
+
     public Chunk getChunkAt(int x, int z) {
         long pos = x;
         pos += z << 16;
         return chunks.get(pos);
     }
-    
+
     public Chunk getChunkAt(Vector3f position) {
         return getChunkAt((int)position.x, (int)position.z);
     }
-    
+
     public Collection<Chunk> getAllChunks() {
         return chunks.values();
     }
-    
-    public boolean save() {
-        
-        return false;
+
+    public String getName() {
+        return name;
     }
-    
+
+    public boolean save(String name) {
+        this.name = name;
+        return save();
+    }
+
+    public boolean save() {
+        File file = new File("levels/"+name+".map");
+        FileOutputStream out = null;
+        ObjectOutputStream stream = null;
+        try {
+            file.createNewFile();
+            out = new FileOutputStream(file);
+            stream = new ObjectOutputStream(out);
+            stream.writeObject(this);
+            return true;
+        } catch(IOException e) {
+            e.printStackTrace();
+            return false;
+        } finally {
+            try {
+                if (stream != null) {
+                    stream.close();
+                }
+                if (out != null) {
+                    out.close();
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
 }
